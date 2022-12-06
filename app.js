25a865cbf0a7b54ff5610377e3bb0e1d05be3809
Hector5280g <hectorgarcia342@yahoo.com>
2022-12-06 11:17:30 -0700

    Working App.js

--- app.js
+++ app.js
@@ -1,18 +1,82 @@
+//variables for the colum on the game board.
+val_c1 = 1
+val_c2 = 1
+val_c3 = 1
+val_c4 = 1
+val_c5 = 1
+val_c6 = 1
+val_c7 = 1
+turn = 1
+
+
+//checking the winner
+
+function check(player) {
+    //Checks for horizontal wins using 4 conditions
+    setTimeout(() => {
+        for (i = 1; i <= 7; i++) {
+            for (j = 1; j <= 3; j++) {
+                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 3}`).style.backgroundColor == `${player}`) {
+                    alert(`${player} wins`)
+                    location.reload()
+                }
+
+            }
+        }
+// Checks for vertical win 
+        for (i = 1; i <= 6; i++) {
+            for (j = 1; j <= 4; j++) {
+                if (document.getElementById(`c${j}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 1}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 2}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 3}r${i}`).style.backgroundColor == `${player}`) {
+                    alert(`${player} wins`)
+                    location.reload()
+                }
+
+            }
+
+        }
+//This checks win for right diagonal win
+        for (i = 1; i <= 4; i++) {
+            for (j = 1; j <= 3; j++) {
+                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 1}r${j + 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 2}r${j + 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 3}r${j + 3}`).style.backgroundColor == `${player}`) {
+                    alert(`${player} wins`)
+                    location.reload()
+                }
+
+            }
+        }
+//This Checks for win for Left diagonal win
+        for (i = 1; i <= 4; i++) {
+            for (j = 6; j >= 4; j--) {
+                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 1}r${j - 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 2}r${j - 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 3}r${j - 3}`).style.backgroundColor == `${player}`) {
+                    alert(`${player} wins`)
+                    location.reload()
+                }
+
+            }
+        }
+
+    }, 200)
+
+}
+
+
+
 //playing
 document.querySelectorAll(".column").forEach((e) => {
     e.addEventListener("click", () => {
 
+        //Eval connects to the ID on the html and the created variables above to make the sum 1
         sum = eval(`val_${e.id}`)
         eval(`val_${e.id}++`)
 
-
+//This IF statement changes the background color of the White circle to red
         if (sum <= 6 && turn % 2 != 0) {
             document.getElementById(`${e.id}r${sum}`).style.backgroundColor = "red"
             turn++
             check('red')
             document.getElementById("whosturn").innerText = "Yellow's Turn"
         }
-        
+        //This Else IF statement changes the background color of the White circle to Yellow.
         else if (sum <= 6 && turn % 2 == 0) {
             document.getElementById(`${e.id}r${sum}`).style.backgroundColor = "yellow"
             turn++
